78f51d935e3b06daea7204192563aaf2074d87df
Parry Choi <43814396+PCRedHot@users.noreply.github.com>
2022-06-24 15:44:07 +0800

    Update sketch.js

--- zine_code_my_life/sketch.js
+++ zine_code_my_life/sketch.js
@@ -1,4 +1,6 @@
-window.open('tel://+85200000000');
+// window.open('tel://+85200000000');
+window.open('https://open.spotify.com/track/0dbKKEitEKGPpgY7qOFwpe');
+// spotify public API key BQC1g1TjIG9Xtjwk226YpgpCUZLv87iIBVI68tWmT5fRkZlgUVLLIQrVMKodLumzp77gCxkHR4S-2Gskeo8H9NDxOpwfMSSIZoNJe7HIMbUlUMoPGI100kRaGMuC7FvgaDNlw9q17_Hix97OjAWSkhdzInCc5ytmRIFt_B_myhkd-AHTEHjNKlHSRewti8J2r40
 
 function setup(){
 
