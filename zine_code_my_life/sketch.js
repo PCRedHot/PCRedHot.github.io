c7ce981e4714c2e440e656309cf505ed882cc266
PCRedHot <43814396+PCRedHot@users.noreply.github.com>
2022-06-26 17:33:32 +0800

    Update sketch.js

--- zine_code_my_life/sketch.js
+++ zine_code_my_life/sketch.js
@@ -1,10 +1,17 @@
 // window.open('tel://+85200000000');
-window.open('spotify:track:0dbKKEitEKGPpgY7qOFwpe');
+try{
+    window.open('spotify:track:0dbKKEitEKGPpgY7qOFwpe');
+}catch (e){
+    console.log(e);
+}
+
 // spotify public API key BQC1g1TjIG9Xtjwk226YpgpCUZLv87iIBVI68tWmT5fRkZlgUVLLIQrVMKodLumzp77gCxkHR4S-2Gskeo8H9NDxOpwfMSSIZoNJe7HIMbUlUMoPGI100kRaGMuC7FvgaDNlw9q17_Hix97OjAWSkhdzInCc5ytmRIFt_B_myhkd-AHTEHjNKlHSRewti8J2r40
 
 function setup(){
-
-
+    createCanvas(windowWidth, windowHeight);
+    background(250);
+    textSize(30);
+    text(`${windowWidth}, ${windowHeight}`, 100, 100);
 }
 
 
