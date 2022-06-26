1f74264780586ed76e414fa0b306e6a7625b3785
PCRedHot <43814396+PCRedHot@users.noreply.github.com>
2022-06-26 17:46:46 +0800

    Update sketch.js

--- zine_code_my_life/sketch.js
+++ zine_code_my_life/sketch.js
@@ -6,8 +6,18 @@
 // }
 
 // spotify public API key BQC1g1TjIG9Xtjwk226YpgpCUZLv87iIBVI68tWmT5fRkZlgUVLLIQrVMKodLumzp77gCxkHR4S-2Gskeo8H9NDxOpwfMSSIZoNJe7HIMbUlUMoPGI100kRaGMuC7FvgaDNlw9q17_Hix97OjAWSkhdzInCc5ytmRIFt_B_myhkd-AHTEHjNKlHSRewti8J2r40
+document.addEventListener('DOMContentLoaded', function () {
+
+});
+
+
+// console.log(document.body.style.padding);
+// document.body.style.margin = "0px";
 
 function setup(){
+    document.body.style.margin = '0px';
+
+
     createCanvas(windowWidth, windowHeight);
     background(250);
     textSize(30);
