3dcd7b9a11bf4901dda45f88849f39fd4b42d521
PCRedHot <43814396+PCRedHot@users.noreply.github.com>
2022-06-26 17:36:38 +0800

    Update sketch.js

--- zine_code_my_life/sketch.js
+++ zine_code_my_life/sketch.js
@@ -1,9 +1,9 @@
 // window.open('tel://+85200000000');
-try{
-    window.open('spotify:track:0dbKKEitEKGPpgY7qOFwpe');
-}catch (e){
-    console.log(e);
-}
+// try{
+//     window.open('spotify:track:0dbKKEitEKGPpgY7qOFwpe');
+// }catch (e){
+//     console.log(e);
+// }
 
 // spotify public API key BQC1g1TjIG9Xtjwk226YpgpCUZLv87iIBVI68tWmT5fRkZlgUVLLIQrVMKodLumzp77gCxkHR4S-2Gskeo8H9NDxOpwfMSSIZoNJe7HIMbUlUMoPGI100kRaGMuC7FvgaDNlw9q17_Hix97OjAWSkhdzInCc5ytmRIFt_B_myhkd-AHTEHjNKlHSRewti8J2r40
 
