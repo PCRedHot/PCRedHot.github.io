9aa37c244a448019119097ec7e69ca56301238eb
PCRedHot <43814396+PCRedHot@users.noreply.github.com>
2022-07-06 22:42:57 +0800

    update api key

--- zine_code_my_life/sketch.js
+++ zine_code_my_life/sketch.js
@@ -7,7 +7,6 @@
 //     console.log(e);
 // }
 
-// spotify public API key BQC1g1TjIG9Xtjwk226YpgpCUZLv87iIBVI68tWmT5fRkZlgUVLLIQrVMKodLumzp77gCxkHR4S-2Gskeo8H9NDxOpwfMSSIZoNJe7HIMbUlUMoPGI100kRaGMuC7FvgaDNlw9q17_Hix97OjAWSkhdzInCc5ytmRIFt_B_myhkd-AHTEHjNKlHSRewti8J2r40
 // document.addEventListener('DOMContentLoaded', function () {
 
 // });
