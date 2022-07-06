c0e9826167c50b4f96f75896cb6bb81284e514d3
PCRedHot <43814396+PCRedHot@users.noreply.github.com>
2022-07-06 23:00:08 +0800

    Update pages.js

--- zine_code_my_life/pages.js
+++ zine_code_my_life/pages.js
@@ -1,8 +1,8 @@
 function loadInitPages() {
-    pages.push(new Page('Hi', font_text, PAGE_WIDTH, PAGE_HEIGHT));
-    pages.push(new Page('開始之前要比你知道\n你輸入既資料係\n*唔會*被伺服器儲存', font_text, PAGE_WIDTH, PAGE_HEIGHT));
+    pages.push(new Page('Hi'));
+    pages.push(new Page('開始之前要比你知道\n你輸入既資料係\n*唔會*被伺服器儲存'));
     pages.push(
-        new Page('所以可以放心\n明白就係下面輸入\"yes\"', font_text, PAGE_WIDTH, PAGE_HEIGHT)
+        new Page('所以可以放心\n明白就係下面輸入\"yes\"')
             .setOnInputCallback((page, v) => {
                 if (v.toLowerCase() == "yes") {
                     page.done = true;
@@ -17,24 +17,23 @@ function loadInitPages() {
 bg_color1 = Color.fromRGBA(30, 215, 96, 200);
 
 function loadAskName(){
-    pages.push(new Page('歡迎你使用Zinder\n全港唯一\n同呢本小誌個作者\n做朋友既交友小誌', font_text, PAGE_WIDTH, PAGE_HEIGHT))
-    pages.push(new Page('我係Parry\n你叫咩名呀?', font_text, PAGE_WIDTH, PAGE_HEIGHT)
+    pages.push(new Page('歡迎你使用Zinder\n全港唯一\n同呢本小誌個作者\n做朋友既交友小誌'))
+    pages.push(new Page('我係Parry\n你叫咩名呀?')
         .setOnInputCallback((page, v) => {
             state['name'] = v;
             page.done = true;
-            pages.push(new Page(`Nice to meet you, ${state['name']}`, font_text, PAGE_WIDTH, PAGE_HEIGHT).setBackgroundColor(bg_color1));
-            pages.push(new Page('問多你一個問題', font_text, PAGE_WIDTH, PAGE_HEIGHT).setBackgroundColor(bg_color1));
+            pages.push(new Page(`Nice to meet you, ${state['name']}`));
+            pages.push(new Page('問多你一個問題'));
             loadAskSpotify();
             Page.nextPage();
         })
-        .setBackgroundColor(bg_color1)
     );
 }
 
 function loadAskSpotify() {
     
     pages.push(
-        new Page('你呢部手機有冇Spotify App?\n請輸入\"yes\"/\"no\"', font_text, PAGE_WIDTH, PAGE_HEIGHT)
+        new Page('你呢部手機有冇Spotify App?\n請輸入\"yes\"/\"no\"')
             .setBackgroundColor(bg_color1)
             .setOnInputCallback((page, v) => {
                 switch (v.toLowerCase()) {
