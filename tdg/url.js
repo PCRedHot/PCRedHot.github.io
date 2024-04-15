09cc152d2824378363cbd54a1580862c31dae24e
Parry Choi <parrychoi1109@gmail.com>
2024-04-15 22:57:28 +0200

    Add more links

--- tdg/url.js
+++ tdg/url.js
@@ -2,3 +2,5 @@ let url = 'https://c73e-202-45-43-19.ngrok-free.app/';
 let cell_count_url = `${url}cell_count.html`;
 let plate_count_url = `${url}plate_count.html`;
 let quiz_url = `${url}classify_quiz.html`;
+let classify_url = `${url}classify.html`;
+let classify_mini_url = `${url}classify_mini.html`;
